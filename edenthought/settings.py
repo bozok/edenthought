0b99fb3f5c8a58cddeaeac522e8a4a47c5c0131b
Ali Fatih AYDIN <bozok17@gmail.com>
2023-10-31 14:41:59 +0300

    Update settings.py
    - allowed hosts & csrf trusted origins updated

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -32,9 +32,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['edenthought-app.onrender.com','*']
 
-#CSRF_TRUSTED_ORIGINS=[]
+CSRF_TRUSTED_ORIGINS=['https://edenthought-app.onrender.com']
 
 
 # Application definition
